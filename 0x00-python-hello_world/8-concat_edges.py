ad936f62316b7ecfdac6ce27ef430aecd80d4bfa
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-28 13:49:45 -0800

    sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\      language that combines remarkable power with clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\      
+language that combines remarkable power with clear syntax"
 str = str[39:66] + " " + str[-17:-13] + " " + str[0:6]
 print(str)
