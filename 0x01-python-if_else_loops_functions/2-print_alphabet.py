75d2a8fb0df6d9c91638f05954335f633f704aac
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-29 09:05:05 -0800

    for loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,5 +3,5 @@ def main():
     #the  (97,123) are ascii representation of lower letters
     for i in range(97,123):
         #the chr() function is used to change the ascii value to character
-        print(chr(i),end= "")
+        print(f"{chr(i)}",end= "")
 main()
