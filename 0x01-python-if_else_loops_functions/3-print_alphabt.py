eb4f47c4ab1f20b9e5a80de3491f3bc6c3b8cf5f
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-29 12:30:51 -0800

    exceptions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def main():
     for i in range(97, 123):
-        if i != 101 and i!= 113:
+        if i != 101 and i != 113:
             print("{}".format(chr(i)), end="")
         else:
             continue
