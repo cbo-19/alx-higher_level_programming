fba45e6c0cb8ac18a93e14516af4bbbcb597a7c4
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-29 09:09:00 -0800

    print string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,5 +3,5 @@ def main():
     #the  (97,123) are ascii representation of lower letters
     for i in range(97,123):
         #the chr() function is used to change the ascii value to character
-        print(f"{chr(i)}",end= "")
+        print("{}".format(chr(i)),end= "")
 main()
