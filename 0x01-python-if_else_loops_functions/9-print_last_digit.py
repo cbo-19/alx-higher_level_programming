a16357c89b9f381d773b97fc60fcce41ded867ab
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-30 00:49:01 -0800

    last

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,7 @@ def print_last_digit(number):
     if number < 0:
         number = number * -1 % 10
         print("{}".format(number), end="")
+        return number
     elif number >= 0:
         number = number % 10
         print(f"{number}", end="")
