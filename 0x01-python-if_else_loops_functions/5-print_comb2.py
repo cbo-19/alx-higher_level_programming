c54f93652c8aca19807f396af6945a293040dfe0
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-30 14:17:48 -0800

    last

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def main():
     for i in range(0, 100):
-        if i < 10:
-            print("0{:d},".format(i), end=" ")
+        if i != 99:
+            print("{:02d},".format(i), end=" ")
         else:
-            print("{:d},".format(i), end=" ")
+            print("99")
 
 
 main()
