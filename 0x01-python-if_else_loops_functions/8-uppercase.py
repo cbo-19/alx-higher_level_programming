986152984113e112c017579fb1f0ffcc9a0c5df8
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-11-30 16:13:08 -0800

    up

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,10 +4,6 @@ def uppercase(str):
         i = ord(i)
         if i >= 97 and i <= 122:
             i -= 32
-            i = chr(i)
-            n = format(i)
-        elif not(i >= 97 and i <= 122):
-            i = chr(i)
-            s = format(i)
-            print("{}".format(s, n), end="")
+        i = chr(i)
+        print("{}".format(i), end='')
     print("")
