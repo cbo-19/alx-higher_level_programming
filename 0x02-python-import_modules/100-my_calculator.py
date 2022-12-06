a27508e0cf1772a17bdb3aa79d67825b37fa9bca
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-12-06 22:16:55 +0000

    calcus

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+!/usr/bin/python3
+if __name__ == "__main__":
+    # IMPORTS
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    # CONDITIONALS
+    if len(argv) != 4:
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
+        print(f"Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    # VARIABLES
+    a = argv[1]
+    b = argv[3]
+
+    # CODE
+    if argv[2] == '+':
+        print(f"{a} + {b} = {add(int(a), int(b))}")
+    if argv[2] == '-':
+        print(f"{a} - {b} = {sub(int(a), int(b))}")
+    if argv[2] == '*':
+        print(f"{a} * {b} = {mul(int(a), int(b))}")
+    if argv[2] == '/':
+        print(f"{a} / {b} = {div(int(a), int(b))}")
