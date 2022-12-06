3cecb09c292abee8a878edc093a849d74061f5d3
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-12-06 22:40:37 +0000

    mag

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 !/usr/bin/python3
-
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
@@ -8,4 +7,4 @@ def magic_calculation(a, b):
             c = add(c, i)
         return c
     else:
-        return (sub(a, b))
+        return sub(a, b)
