3b98893dacb201f5eb629dfcae3832d7dc612a57
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-12-06 02:42:30 -0800

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = -1
-    while abs(i) <= len(my_list):
+    while abs(i) < len(my_list):
         print("{:d}".format(my_list[i]))
         i -= 1
