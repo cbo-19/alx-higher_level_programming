ad7a070b328c6e79c5ede9c602c2ea7fe9975a61
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-12-06 01:17:44 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    x = my_list
-    for i in range(len(x)):
-        i = idx
-        if i >= 0 and i <= len(x):
-            return x[i]
-            break
-        elif i < 0 or i > len(x):
-            return None
+    if i < 0 and i > len(my_list):
+        return None
+    else:
+        return my_list[idx]
