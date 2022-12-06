a05a4ee959dd1bb429f78a64c57c025adcf806a3
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-12-06 06:36:13 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        maptr = 't'.join(map(str, i))
-        print("{}".format(maptr))
+        print(' '.join("{:d}".format(element) for element in i))
