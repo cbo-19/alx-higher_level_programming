fef2c18f887dc5150c62a2f37410659703ac817b
cbo-19 <111020956+cbo-19@users.nonreply.github.com>
2022-12-06 01:51:57 -0800

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        i = idx
-        if i < 0 or i >len(my_list):
-            return my_list
-        else:
-            my_list[i] = element
-            a = my_list
-            return a
-
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        a = my_list
+        return a
