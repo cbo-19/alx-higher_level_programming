b4259ded956314f2630b48e69e1e23d9c151423b
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-12-06 22:34:23 +0000

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    addition = 0
-
-    for i in range(1, len(sys.argv)):
-        addition = addition + int(sys.argv[i])
-
-    print(addition)
+    from sys import argv
+    print("{}".format(sum(int(a) for a in argv[1:]))
