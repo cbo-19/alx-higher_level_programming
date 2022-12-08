ff8041bc0d613f7760718189006088b167e35fd9
cbo-19 <111020956+cbo-19@users.noreply.github.com>
2022-12-08 11:55:22 +0000

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary):
-        print(f"{key:} {a_dictionary[key]}")
+    for value in sorted(a_dictionary.keys()):
+        print(f"{value:} {a_dictionary[value]}")
